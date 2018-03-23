ebc9d99c922ffa85abb06a57bf90344738c8d4aa
denny <you@example.com>
2018-03-22 23:20:29 -0400

    website update

---
+++ info.js
@@ -0,0 +1,5 @@
+var name = "Your Name Here";
+
+document.write("<span id ='footer'>JDenny's Website</span><br />&copy; Denny Velkova<br />
+  Modified March 22 2018<br />
+  Retrieved: <script>document.write(Date());</script>");
